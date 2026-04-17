2818ed556eea2e9f27001c305aaca28d1dc49cdb
waqasAnjum1985 <anjum.pharmacy@gmail.com>
2026-04-17 09:59:37 +0500

    ok

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="py-2">
-      <div className="h-[1px] w-full bg-gray-700 -mb-8 md:mt-10 "></div>
+    <div className="py-2 md:mt-10 ">
+      <div className="h-[1px] w-full bg-gray-700 -mb-8 md:mt-10"></div>
       <div className="flex items-center justify-center flex-col mt-10  opacity-70">
         <h1 className="text-white">Designed & Developed By</h1>
         <h1 className="text-white">
