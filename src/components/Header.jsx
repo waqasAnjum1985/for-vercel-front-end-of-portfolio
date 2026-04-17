46911e2de7e53866a607a885005223f71ae74516
waqasAnjum1985 <anjum.pharmacy@gmail.com>
2026-04-17 10:27:35 +0500

    fixing header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="sticky top-0  p-5  bg-primary flex justify-between">
+    <div className="sticky top-0 md:mb-10  p-5  bg-primary flex justify-between">
       <h1 className="text-secondary ml-1 text-4xl   font-semibold">Dr.</h1>
       <h1 className="text-white   text-4xl  font-semibold">W</h1>
       <h1 className="text-tertary mr-10   text-4xl  font-semibold">A</h1>
